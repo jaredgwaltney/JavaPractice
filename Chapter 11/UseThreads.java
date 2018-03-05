ae36bbad27482d049ab8d8390177bd311a013165
jaredgwaltney <jared.gwaltney@gmail.com>
2018-03-04 22:42:49 -0500

    Added chapters 11, 16 and 17.  Chapters 12-15 inconsequential.

---
+++ Chapter 11/UseThreads.java
@@ -0,0 +1,48 @@
+/* 
+  Threading example
+*/
+
+class MyThread implements Runnable {
+	Thread thrd;
+	
+	MyThread(String name) {
+		thrd = new Thread(this, name);
+	}
+	
+	public static MyThread createAndStart(String name) {
+		MyThread myThrd = new MyThread(name);
+		myThrd.thrd.start();
+		return myThrd;
+	}
+	
+	public void run() {
+		System.out.println(thrd.getName() + " starting.");
+		try {
+			for (int count=0; count<10; count++) {
+				Thread.sleep(400);
+				System.out.println("In " + thrd.getName() + ", count is " + count);
+			}
+		} catch(InterruptedException ex) {
+			System.out.println(thrd.getName() + " interrupted.");
+		}
+		System.out.println(thrd.getName() + " terminated.");
+	}
+}
+
+class UseThreads {
+	public static void main(String args[]) {
+		System.out.println("Main thread starting.");
+		
+		MyThread mt = MyThread.createAndStart("Child #1");
+		
+		for (int i=0; i<50; i++) {
+			System.out.print(".");
+			try {
+				Thread.sleep(100);
+			} catch(InterruptedException ex) {
+				System.out.println("Main thread interrupted.");
+			}
+		}
+		System.out.println("Main thread ending.");
+	}
+}
---
+++ Chapter 17/JavaFXEventDemo.java
@@ -0,0 +1,46 @@
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.event.*;
+import javafx.geometry.*;
+
+public class JavaFXEventDemo extends Application {
+	Label response;
+	
+	public static void main(String args[]) {
+		launch(args);
+	}
+	
+	public void start(Stage myStage) {
+		myStage.setTitle("Use JavaFX Buttons and Events.");
+		
+		FlowPane rootNode = new FlowPane(10, 10);
+		rootNode.setAlignment(Pos.CENTER);
+		
+		Scene myScene = new Scene(rootNode, 300, 100);
+		
+		myStage.setScene(myScene);
+		
+		response = new Label("Push a Button.");
+		Button btnUp = new Button("Up");
+		Button btnDown = new Button("Down");
+		
+		btnUp.setOnAction(new EventHandler<ActionEvent> () {
+			public void handle(ActionEvent ae) {
+				response.setText("You pressed up.");
+			}
+		});
+		
+		btnDown.setOnAction(new EventHandler<ActionEvent> () {
+			public void handle(ActionEvent ae) {
+				response.setText("You pressed down.");
+			}
+		});
+		
+		rootNode.getChildren().addAll(btnUp,btnDown,response);
+		
+		myStage.show();
+	}
+}
